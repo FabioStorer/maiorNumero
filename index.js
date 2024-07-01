4e2eb3c584226d98a5c0bb60db4d673a8a0e5672
Fabio.Storer <fabiostorer19@gmail.com>
2024-07-01 17:38:07 -0300

    feat: codigo funcional

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const prompt = require ('prompt-sync')();
+const prompt = require('prompt-sync')();
 
-let numero = prompt ('Olá. Vou retornar para você o maior número possível \nque pode ser feito com os algarismos que você fornecer:');
+let numero = prompt(`Olá. Vou retornar para você o maior número possível que pode ser feito com os algarismos que você fornecer. Digite um número: `).trim();
+
+if (numero < 10) {
+    console.log(`O maior número possível que pode ser gerado com o número informado é `, numero, `.`);
+    process.exit();
+}
+
+numero = numero.split('');
+
+for (let i = 0; i < numero.length; i++) {
+    for (let j = 0; j < numero.length; j++) {
+        if (numero[i] > numero[j]) {
+            let aux = numero[i];
+            numero[i] = numero[j];
+            numero[j] = aux;
+        }
+    }
+}
+
+numero = numero.join('');
+console.log(`O maior número possível que pode ser gerado com o número informado é `, numero, `.`);
